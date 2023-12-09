273cb5847383f5447a49028e330b15c87ea95b6e
MOHAMMAD IMRAN KHAN <124260776+mdimrankhan175@users.noreply.github.com>
2023-12-09 08:08:01 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,39 @@
-import './index.css'
+import React from 'react';
+import { FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa'; // Importing icons from react-icons
+import './Header.css'; // You can create a separate CSS file for the Header component
 
 const Header = () => (
   <nav className="nav-header">
     <div className="nav-content">
-      <p className="website-logo">RA</p>
+      {/* Logo image at the left end */}
+      <div className="logo-container">
+        <img
+          className="website-logo"
+          src="https://res.cloudinary.com/dbelvmspz/image/upload/v1660558582/imran_logo_zh0c8u.jpg"
+          alt="Logo"
+        />
+      </div>
+
+      {/* Navigation elements at the right end */}
       <ul className="nav-menu">
         <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-linkedin-img.png"
-            alt="Linked In"
-          />
+          <a href="https://www.linkedin.com/in/imrankhanmohammad/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="social-network-icon" />
+          </a>
         </li>
         <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-github-img.png"
-            alt="Git Hub"
-          />
+          <a href="https://github.com/mdimrankhan175/" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="social-network-icon" />
+          </a>
         </li>
         <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-twitter-img.png"
-            alt="Twitter"
-          />
+          <a href="https://www.youtube.com/channel/UCkONUJKvEVz45urLxXIJ9YQ" target="_blank" rel="noopener noreferrer">
+            <FaYoutube className="social-network-icon" />
+          </a>
         </li>
       </ul>
     </div>
   </nav>
-)
+);
 
-export default Header
+export default Header;
